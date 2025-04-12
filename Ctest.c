d91b29e35d45f3197473e717ab695aa9a8905f26
Cpt.Mitchell <695046612@qq.com>
2025-04-12 17:25:05 +0800

    4.12

--- Ctest.c
+++ Ctest.c
@@ -1,6 +1,77 @@
 ﻿#include <stdio.h>
 #include <string.h>
+#include <stddef.h>
+#include <malloc.h>
 // #define FORMAT "%d,%d\n" // 宏替换
+#define FORMAT "%d\n%s\n%f\n%f\n%f\n"
+#define LEN sizeof(struct dynamicList) // sizeof为求字节数运算符
+struct person                          // 声明结构体
+{
+    char name[20];
+    int count;
+} leader[3] = {"Li", 0, "Zhang", 0, "Liu", 0};
+
+struct student
+{
+    int num;
+    char name[20];
+    float score[3];
+} stu = {12345, "Li Li", 67.5, 89, 78.6};
+
+struct list
+{
+    long num;
+    float score;
+    struct list *next;
+};
+
+// 建立动态链表
+struct dynamicList
+{
+    long num;
+    float score;
+    struct dynamicList *next;
+};
+int listNum;                    // listNum为全局变量，文本文件模块中各函数均可使用
+struct dynamicList *creat(void) // 定义函数，此函数返回一个指向链表头的指针
+{
+    struct dynamicList *head;    // 声明head为结构体dynamicList的头地址指针变量
+    struct dynamicList *p1, *p2; // 声明p1与p2为指向dynamicList的指针变量
+    listNum = 0;
+    // 开辟一个新单元
+    // malloc返回的是不指向任何类型数据的指针（void *）
+    p1 = p2 = (struct dynamicList *)malloc(LEN); // 而p1、p2是指向struct dynamicList类型数据的指针变量，因此必须用强制类型转换的方法使指针的基类型改变为struct dynamicList类型
+    scanf("%d,%f", &p1->num, &p1->score);
+    head = NULL;
+    while (p1->num != 0)
+    {
+        listNum = listNum + 1;
+        if (listNum == 1)
+            head = p1; // 第一次循环时赋给head头地址
+        else
+            p2->next = p1;
+        p2 = p1;
+        p1 = (struct dynamicList *)malloc(LEN);
+        scanf("%d,%f", &p1->num, &p1->score);
+    } // p1指向新节点，p2指向链表最后一个结点，把p1所指的结点连接在p2所指的结点后，用p2->next=p1来实现
+    p2->next = NULL; // 最后使p2指向空结点结束
+    return (head);   // head已定义为指针变量，指向struct dynamicList类型数据，因此返回的是链表的头地址
+}
+
+// 输出链表
+void printList(struct dynamicList *head)
+{
+    struct dynamicList *p;
+    printf("\nNow,These %d record are:\n", listNum);
+    p = head;
+    if (head != NULL)
+        do
+        {
+            printf("%d%8.1f\n", p->num, p->score);
+            p = p->next;
+        } while (p != NULL);
+}
+
 void main()
 {
     // 二维数组地址指针
@@ -127,16 +198,16 @@ void main()
     // }
 
     // 使用指向指针的指针（字符串数组）
-    char *name[] = {"Follow me", "BASIC", "Great Wall", "FORTRAN", "Computer design"};
-    char **p; // 指针变量p是指向一个字符指针变量（即指向字符型数据的指针变量），p是指向指针的指针变量
-    int i;
-    for (i = 0; i < 5; i++)
-    {
-        p = name + i;        // 使p指向name数组的i号元素name[i]
-        printf("%p\n", p);   // p为指针变量，指向name数组的i号元素
-        printf("%s\n", *p);  // *p是name[i]的值
-        printf("%d\n", **p); // **p为字符串数组name[i][0]的ASCII值
-    }
+    // char *name[] = {"Follow me", "BASIC", "Great Wall", "FORTRAN", "Computer design"};
+    // char **p; // 指针变量p是指向一个字符指针变量（即指向字符型数据的指针变量），p是指向指针的指针变量
+    // int i;
+    // for (i = 0; i < 5; i++)
+    // {
+    //     p = name + i;        // 使p指向name数组的i号元素name[i]
+    //     printf("%p\n", p);   // p为指针变量，指向name数组的i号元素
+    //     printf("%s\n", *p);  // *p是name[i]的值
+    //     printf("%d\n", **p); // **p为字符串数组name[i][0]的ASCII值
+    // }
 
     // 指向整型数据或实型数据
     // int a[5] = {1, 3, 5, 7, 9};
@@ -150,6 +221,86 @@ void main()
     // p = num + 2; // 指针数组的元素只能存放地址
     // printf("%p\n", *p);  // *p是p指向num数组中各元素的地址，也就是间接指向a数组中各元素的地址
     // printf("%d\n", **p); // **P是p指向num数组中各元素的值，也就是间接指向a数组中各元素的值
+
+    // 结构体应用（结构体可以声明在main函数外）
+    // int i, j;
+    // char leader_name[20]; // 声明输入名
+    // for (i = 0; i < 5; i++)
+    // {
+    //     scanf("%s", leader_name);
+    //     for (j = 0; j < 3; j++)
+    //         if (strcmp(leader_name, leader[j].name) == 0) // 输入名与结构体内名字相比，如全等则相对结构体内count元素++
+    //             leader[j].count++;
+    // }
+    // printf("\n");
+    // for (i = 0; i < 3; i++)
+    //     printf("%5s:%d\n", leader[i].name, leader[i].count); // 循环输出结构体内各元素
+
+    // 指向结构体变量的指针的应用
+    // struct student
+    // {
+    //     long num;
+    //     char name[20];
+    //     char sex;
+    //     float score;
+    // };
+    // struct student stu_1;
+    // struct student *p; // 声明p为指向student结构体的指针变量
+    // p = &stu_1;        // 将结构体stu_1的首地址赋给指针变量p，使其能够指向stu_1结构体
+    // stu_1.num = 89101;
+    // strcpy(stu_1.name, "Li Lin"); // 字符串复制
+    // stu_1.sex = 'M';              // 这里的'M'为字符，需要用%c输出而不能用%s输出
+    // stu_1.score = 89.5;
+    // printf("No.:%1d\nname:%1s\nsex:%1c\nscore:%1f\n", stu_1.num, stu_1.name, stu_1.sex, stu_1.score);
+    // printf("No.:%1d\nname:%1s\nsex:%1c\nscore:%1f\n", (*p).num, (*p).name, (*p).sex, (*p).score);
+    // printf("No.:%1d\nname:%1s\nsex:%1c\nscore:%1f\n", p->num, p->name, p->sex, p->score); // 可以用->表示p所指向的结构体变量中的各成员
+
+    // 指向结构体数组的指针
+    // struct student
+    // {
+    //     int num;
+    //     char name[20];
+    //     char sex;
+    //     int age;
+    // };
+    // struct student stu[3] =
+    //     {
+    //         {10101, "Li Lin", 'M', 18},
+    //         {10102, "Zhang Fun", 'M', 19},
+    //         {10104, "Wang Min", 'F', 20}};
+    // struct student *p; // 声明p为指向student结构体的指针变量
+    // printf("  No.     Name         sex        age\n");
+    // for (p = stu; p < stu + 3; p++) // 将stu数组的首地址赋给指针变量p，使p可以指向stu数组
+    //     printf("%5d%10s%10c%12d\n", p->num, p->name, p->sex, p->age); // (++p)->num使p自加1，然后得到指向元素中num成员值；(p++)->num是先得到p->num的值，然后使p自加1指向stu[1]
+
+    // 用结构体变量作函数参数（指针）
+    // void printStu(struct student *); // 形参类型为指向结构体的指针变量
+    // printStu(&stu);                  // 实参为stu的起始地址，传参时将stu的起始地址传给指针变量p，这样就能使p指向stu
+
+    // 建立和输出链表（静态链表）
+    // struct list a, b, c, *head, *p;
+    // a.num = 10101;
+    // a.score = 89.5;
+    // b.num = 10103;
+    // b.score = 90;
+    // c.num = 10107;
+    // c.score = 85;
+    // head = &a;     // 将结点a的起始地址赋给头指针head
+    // a.next = &b;   // 将结点b的起始地址赋给a结点的next成员
+    // b.next = &c;   // 将结点c的起始地址赋给b结点的next成员
+    // c.next = NULL; // c结点的next成员不存放其他结点地址
+    // p = head;      // 使p指针指向a结点（头指针）
+    // do
+    // {
+    //     printf("%1d%5.1f\n", p->num, p->score); // 输出p指向的结点的数据
+    //     p = p->next;                            // 使p指向下一结点
+    // } while (p != NULL); // 输出完c结点后p值为NULL
+    // // do while相当于until事件，当····时结束循环
+
+    // 建立并输出动态链表
+    struct dynamicList *listHead;
+    listHead = creat();
+    printList(listHead);
 }
 
 // 求a和b中的大者max函数
@@ -212,4 +363,11 @@ void print(char *name[], int n)
     int i;
     for (i = 0; i < n; i++)
         printf("%s\n", name[i]);
+}
+
+// 结构体变量作函数参数
+void printStu(struct student *p) // 注意struct student声明要写在main函数外部，否则此处调用不到会报错 // 注意形参类型
+{
+    printf(FORMAT, p->num, p->name, p->score[0], p->score[1], p->score[2]); // 用指针变量调用各成员的值
+    printf("\n");
 }
